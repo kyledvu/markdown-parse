d0551e32bc18afaadf2e366762f145604281a323
kyledvu <kdvu@ucsd.edu>
2022-01-27 15:06:09 -0800

    added provided test files, added fix to avoid infinite loop

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.io.IOError;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,11 +15,33 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException {
+    public void testMyFile1() throws IOException {
         Path fileName = Path.of("C:/Users/Kyle/Documents/GitHub/markdown-parse/test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
 
         assertEquals(List.of("https://something.com", "some-page.html"), links);
     }
+
+    @Test
+    public void testMyFile2() throws IOException {
+	String contents = Files.readString(Path.of("C:/Users/Kyle/Documents/GitHub/markdown-parse/test2.md"));
+	List<String> expect = List.of("https://duckduckgo.com", "https://kde.org");
+	assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testMyFile3() throws IOException {
+	String contents = Files.readString(Path.of("C:/Users/Kyle/Documents/GitHub/markdown-parse/test3.md"));
+	List<String> expect = List.of();
+	assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testMyFile4() throws IOException {
+	String contents = Files.readString(Path.of("C:/Users/Kyle/Documents/GitHub/markdown-parse/test4.md"));
+	List<String> expect = List.of();
+	assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
 }
