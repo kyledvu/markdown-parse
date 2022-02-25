5dfb233f603d596c26e4ddaf114b82aa517690a5
kyledvu <kdvu@ucsd.edu>
2022-02-24 17:07:34 -0800

    added tests for reviewed MDParse

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -102,4 +102,31 @@ public class MarkdownParseTest {  // class header
 
         assertEquals(List.of("https://ucsd-cse15l-w22.github.io/"), links);
     }
+
+    @Test
+    public void testReviewedSnippetOne() throws IOException {
+        Path fileName = Path.of("./test-snippet1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = NewMDParse.getLinks(contents);
+
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), links);
+    }
+
+    @Test
+    public void testReviewedSnippetTwo() throws IOException {
+        Path fileName = Path.of("./test-snippet2.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = NewMDParse.getLinks(contents);
+
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), links);
+    }
+
+    @Test
+    public void testReviewedSnippetThree() throws IOException {
+        Path fileName = Path.of("./test-snippet3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = NewMDParse.getLinks(contents);
+
+        assertEquals(List.of("https://ucsd-cse15l-w22.github.io/"), links);
+    }
 }
